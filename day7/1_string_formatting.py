a17162a42e9dc06d2bac8d939f0bd544a72d5e85
npaudyal <npaudyal321@gmail.com>
2024-03-14 17:59:47 +0545

    dict

---
+++ day7/1_string_formatting.py
@@ -0,0 +1,11 @@
+# f-strings
+
+language = "Python"
+print(f"I am learning {language}")
+
+
+# using .format() method
+
+messg = "I am learning {}. I am also learning {}".format("Python", "Java")
+print(messg)
+
